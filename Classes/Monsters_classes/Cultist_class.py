91f86282e6e16849fd56bff5c74509b2bdfb812e
Ilay_(Sleepy Mita) <148774440+Ilay4133@users.noreply.github.com>
2025-11-09 23:31:47 +0700

    Create Cultist_class.py

---
+++ Classes/Monsters_classes/Cultist_class.py
@@ -0,0 +1,17 @@
+from OOP_DnD_like_game.Classes.Monster_class import *
+
+
+class Cultist(Monster):
+    def __init__(self, level):
+        monster_info = "Культист"
+        super().__init__(
+            level=level,
+            name=f"Культист ({level})",
+            magical_level=level,
+            max_hp=78 * level,
+            atk=30 * level,
+            defense=0 * level,
+            loot={"монеты": [12, 0.7],
+                  "обрывки одежды культиста": {"количество": 13, "шанс выпадения": 1, "стоимость": 1,
+                                               "описание": "___"},
+                  "шапка культиста": {"количество": 1, "шанс выпадения": 0.3, "стоимость": 1, "описание": "___"}})
