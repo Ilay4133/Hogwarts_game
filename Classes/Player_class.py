abc1c0b88721947657646ba50e9c72f8b91d401f
Ilay_(Sleepy Mita) <148774440+Ilay4133@users.noreply.github.com>
2025-11-09 23:33:17 +0700

    Add files via upload

---
+++ Classes/Player_class.py
@@ -0,0 +1,101 @@
+import threading
+import time
+from tqdm import tqdm
+
+
+class Player:
+    def __init__(self, level, name, magical_level, max_hp, atk, defense, money):
+        self.level = level
+        self.name = name
+        self.magical_level = magical_level
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.atk = atk
+        self.defens = defense
+        self.inventory = {}
+        self.money = money
+        self.h_timer: bool = True
+
+    def attack(self, opponent, defense_bool, coef) -> list:
+        if defense_bool != 0:
+            damage = self.atk * coef - opponent.defens
+            if damage < 0:
+                opponent.hp = opponent.hp + damage
+                return [round(opponent.hp, 2), -damage]
+            else:
+                opponent.hp = opponent.hp - damage
+                return [round(opponent.hp, 2), damage]
+        else:
+            damage = self.atk * coef
+            opponent.hp = opponent.hp - damage
+            return [round(opponent.hp, 2), damage]
+
+    def healing(self) -> list or None:
+        healing_time = 15
+
+        def timer():
+            self.h_timer = True
+
+        if self.h_timer:
+            healing_hp = self.hp + 30
+            if healing_hp > self.max_hp:
+                self.hp = self.max_hp
+                self.h_timer = False
+            else:
+                self.hp = self.hp + 30
+                self.h_timer = False
+            thread = threading.Timer(healing_time, timer)
+            thread.start()
+            return [round(self.hp, 2), healing_time]
+        else:
+            return None
+
+    @staticmethod
+    def defensing() -> int:
+        return 1
+
+    def see_inventory(self) -> None:
+        print("\n|________________________________________|")
+        print(f"Монеты: {self.money}")
+        print("|________________________________________|")
+        for key, value in self.inventory.items():
+            print(f"| {key.upper()} |")
+            for v_key, v_value in value.items():
+                if v_key != "type":
+                    print(f"| {v_key} - {v_value}")
+                else:
+                    pass
+            print("|________________________________________|\n")
+        return None
+
+    def campfiring(self, campfire_time=10) -> None:
+        print(f"Вы у костра, подождите {campfire_time} секунд чтобы восстановить здоровье")
+        for i in tqdm(range(100)):
+            time.sleep(campfire_time / 100)
+        print("Вы посидели у костра, ваше здоровье восстановилось")
+        self.hp = self.max_hp
+        return None
+
+    def open_inventory(self):
+        print("\n|________________________________________|")
+        print(f"Монеты: {self.money}")
+        print("|________________________________________|")
+        for key, value in self.inventory.items():
+            print(f"| {key.upper()} |")
+            for v_key, v_value in value.items():
+                if v_key != "type":
+                    print(f"| {v_key} - {v_value}")
+                else:
+                    pass
+            print("|________________________________________|")
+        invent_chose = str(input("Что использовать или выйти - Q\n> "))
+        chose_data = self.inventory[invent_chose]
+        if chose_data["type"][0] == "heal":
+            print("heal")
+            healing_hp = chose_data["type"][1]
+            if self.hp + healing_hp > self.max_hp:
+                self.hp = self.max_hp
+            else:
+                self.hp += healing_hp
+            print(f"Вы восстановили {healing_hp} ХП, \n"
+                  f"ХП сейчас: {self.hp}\n_____________________")
