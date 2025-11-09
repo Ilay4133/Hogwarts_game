4c726ee165a6b0193e4a5c4d931255c69bab95de
Ilay_(Sleepy Mita) <148774440+Ilay4133@users.noreply.github.com>
2025-11-09 23:37:07 +0700

    Add files via upload

---
+++ Assets.py
@@ -0,0 +1,21 @@
+from OOP_DnD_like_game.Classes.Shop_class import *
+from OOP_DnD_like_game.Classes.Monsters_classes.Drow_class import *
+from OOP_DnD_like_game.Classes.Monsters_classes.Sceleton_class import *
+from OOP_DnD_like_game.Classes.Monsters_classes.Cultist_class import *
+from OOP_DnD_like_game.Classes.Monsters_classes.Babau_class import *
+from OOP_DnD_like_game.Classes.Monsters_classes.Bone_Naga_Spirit_class import *
+from OOP_DnD_like_game.Classes.Monsters_classes.Smoke_Mephisto_class import *
+from OOP_DnD_like_game.Classes.Monsters_classes.Dragon_class import *
+
+
+sceleton = Sceleton(1)
+cultist = Cultist(1)
+babau = Babau(1)
+bone_naga_spirit = Bone_Naga_Spirit(1)
+drow = Drow(1)
+smoke_mephisto = Smoke_Mephisto(1)
+dragon = Dragon(1)
+# __________
+shop = Shop()
+
+all_monsters_list = [sceleton, cultist, babau, bone_naga_spirit, dragon,drow, smoke_mephisto]
---
+++ Main.py
@@ -0,0 +1,46 @@
+from OOP_DnD_like_game.Game_functions.Battle_func import *
+from OOP_DnD_like_game.Game_functions.Shoping_func import *
+from OOP_DnD_like_game.Game_functions.New_player_create_func import *
+
+
+def main(start) -> int:
+    global main_Player
+    if start == 1:
+        print("Приветствуем вас в мире меча и магии!")
+        main_Player = new_player_create()
+        start = 0
+    while True:
+        main_select = str(input("\nЧто хотите сделать? \n"
+                                "A - Cразиться\n"
+                                "S - Магазин\n"
+                                "C - Пойти к костру\n"
+                                "I - Инвентарь\n"
+                                "Q - Выход\n"
+                                "> "))
+        up_main_select = main_select.upper()
+        if up_main_select == "A":
+            if main_Player.hp <= 0:
+                print("Ваше здоровье низкое, вы не можете вступить в бой.")
+            else:
+                battle(main_Player)
+        elif up_main_select == "S":
+            shopping(main_Player, shop)
+        elif up_main_select == "C":
+            if main_Player.hp <= 0:
+                main_Player.campfiring()
+            else:
+                print("Ваше здоровье в порядке")
+
+        elif up_main_select == "I":
+            main_Player.see_inventory()
+        elif up_main_select == "Q":
+            break
+        else:
+            pass
+
+    return 0
+
+
+if __name__ == '__main__':
+    start = 1
+    main(start)
